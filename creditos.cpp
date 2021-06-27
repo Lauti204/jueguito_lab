0d7f6350a6a3b6b366aff3f84ebd216df2288e95
Natalids <67949720+Natalids@users.noreply.github.com>
2021-06-28 00:13:23 +0200

    Update creditos.cpp

--- creditos.cpp
+++ creditos.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void){
   setlocale(LC_ALL, "Spanish");
-
+ HOLA
   cout << "EQUIPO 'NIDEAH'" << endl << endl;
   cout << "LEGAJO          APELLIDO Y NOMBRE" << endl;
   cout << " 24960          Della Savia Natalí" << endl;
